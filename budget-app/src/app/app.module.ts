357a78c4b854cfd2f874e4d67fcdf36b264f3183
Mert <mert1ozturk@gmail.com>
2020-12-22 13:32:24 +0300

    form init

--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    AddItemFormComponent
   ],
   imports: [
     BrowserModule,
