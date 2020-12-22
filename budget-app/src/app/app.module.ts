2a7e09db5832ff6e509cc39efef2ad1589499340
Mert <mert1ozturk@gmail.com>
2020-12-22 14:19:16 +0300

    budget item added

--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent,
+    BudgetItemCardComponent
   ],
   imports: [
     BrowserModule,
